177a8d6e4c006c114523eeff2087cb7928122418
assermoussa <asser.y.moussa@gmail.com>
2024-01-22 19:44:30 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+const event = "Birthday";
+function writeCards(names, event){
+    const namearr = [];
+    for (let i = 0; i < names.length; i++) {
+        namearr[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        //debugger;
+      }
+      return namearr;
+};
+
+let num = 10;
+function countDown(x){
+    while (x > -1) {
+        console.log(x--);
+      } 
+}
